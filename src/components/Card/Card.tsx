c83daa6c0a33839c688a43c34b92501ef0ca5d15
Odinokun <odinokun@gmail.com>
2022-06-12 00:21:39 +0200

    did add products to cart

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,5 +1,7 @@
 import React, {useState} from "react";
 import s from "./card.module.scss";
+import {log} from 'util';
+import {cardObj} from '../../App';
 
 export type CardType = {
   id: number
@@ -7,28 +9,34 @@ export type CardType = {
   title: string
   price: number
   onClickFavorite: () => void
+  onPlus: ({id, imgUrl, title, price}: cardObj) => void
 }
 
-export const Card = (props: CardType) => {
+export const Card = ({id, imgUrl, title, price, onClickFavorite, onPlus}: CardType) => {
   const [isAdded, setIsAdded] = useState(false);
 
+  // added sneakers to card after cross click
+  // and change button color
   const onClickPlus = () => {
-    setIsAdded(!isAdded)
+    onPlus({id, imgUrl, title, price});
+    setIsAdded(!isAdded);
   }
 
   return (
     <div className={s.card}>
-      <button onClick={props.onClickFavorite} className={s.favorite}>
+      <button onClick={onClickFavorite} className={s.favorite}>
         <img src="/img/heart-unliked.svg" alt="unliked"/>
       </button>
-      <img className={s.cardImage} src={props.imgUrl} alt="sneakers"/>
-      <h5 className={s.cardTitle}>{props.title}</h5>
+      <img className={s.cardImage} src={imgUrl} alt="sneakers"/>
+      <h5 className={s.cardTitle}>{title}</h5>
       <div className={s.cardInfo}>
         <div className={s.cardPrice}>
           <span>Price:</span>
-          <b>$ {props.price}</b>
+          <b>$ {price}</b>
         </div>
-        <button className={s.button} onClick={onClickPlus}>
+        <button
+          className={s.button}
+          onClick={onClickPlus}>
           <img src={isAdded ? '/img/btn-checked.svg' : '/img/btn-plus.svg'} alt="plus"/>
         </button>
       </div>
