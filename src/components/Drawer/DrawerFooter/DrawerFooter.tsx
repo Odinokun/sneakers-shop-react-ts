32ad81a480104f27aa8dcb48a2346422e2f1176d
Odinokun <odinokun@gmail.com>
2022-07-05 15:09:11 +0200

    add total price to drawer

--- src/components/Drawer/DrawerFooter/DrawerFooter.tsx
+++ src/components/Drawer/DrawerFooter/DrawerFooter.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import s from './drawerFooter.module.scss';
+import AppContext from '../../../context';
+import {CardObj} from '../../../App';
 
 type PropsType = {}
 
 export const DrawerFooter = (props: PropsType) => {
+  const {cartItems} = React.useContext<any>(AppContext);
+  const totalPrice = cartItems.reduce((sum: number, obj: CardObj) => +obj.price + +sum, 0);
+
   return (
     <>
       <ul className={s.drawerFooter}>
         <li className={s.drawerFooterItem}>
           <span className={s.drawerFooterTitle}>Total:</span>
           <div className={s.drawerFooterDecor}></div>
-          <div className={s.drawerFooterPrice}>$ 3541</div>
+          <div className={s.drawerFooterPrice}>$ {totalPrice}</div>
         </li>
         <li className={s.drawerFooterItem}>
           <span className={s.drawerFooterTitle}>Bonus 10%:</span>
           <div className={s.drawerFooterDecor}></div>
-          <div className={s.drawerFooterPrice}>$ 35</div>
+          <div className={s.drawerFooterPrice}>$ {totalPrice + (totalPrice / 10)}</div>
         </li>
       </ul>
       <button className={s.drawerFooterBtn}>
